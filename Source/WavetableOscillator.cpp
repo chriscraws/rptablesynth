5d259ecc15a82406ff38fa3ba1c72a685c9c0c4c
kbruccoleri <kevinbruccoleri@gmail.com>
2015-01-06 03:48:58 -0500

    start reading in from the sample files

--- Source/WavetableOscillator.cpp
+++ Source/WavetableOscillator.cpp
@@ -10,11 +10,55 @@
 
 #include "WavetableOscillator.h"
 
-WavetableOscillator::WavetableOscillator() :    baseHz(110.0),
-                                                wavetableSampleRate(44100),
-                                                level("level", 0, 1, 1),
-                                                position("position", 0, 127, 0),
-                                                wavetable(128, std::vector<float>(401)) {}
+WavetableOscillator::WavetableOscillator() :
+        baseHz(110.0),
+        wavetableSampleRate(44100),
+        level("level", 0, 1, 1),
+        position("position", 0, 127, 0),
+        wavetable(128, std::vector<float>(401))
+{
+    
+    const int MAX_CHARS_PER_LINE = 512;
+    //const int MAX_TOKENS_PER_LINE = 20;
+    //const char* const DELIMITER = " ";
+                                                    
+    std::ifstream fsA;
+    std::ifstream fsB;
+    fsA.open("A.txt");
+    fsB.open("B.txt");
+    AudioSampleBuffer bufferA;
+    bufferA = AudioSampleBuffer();
+    AudioSampleBuffer bufferB;
+    bufferB = AudioSampleBuffer();
+    
+    // traverse the entire first file
+    while (!fsA.eof()) {
+        
+        // read an entire line into memory
+        char buf[MAX_CHARS_PER_LINE];
+        fsA.getline(buf, MAX_CHARS_PER_LINE);
+        
+        String lineString = String(buf);
+        lineString.getFloatValue();
+    
+    }
+    
+    // traverse the entire second file
+    while (!fsB.eof()) {
+
+        // read an entire line into memory
+        char buf[MAX_CHARS_PER_LINE];
+        fsB.getline(buf, MAX_CHARS_PER_LINE);
+
+        String lineString = String(buf);
+        lineString.getFloatValue();
+
+    }
+
+    fsA.close();
+    fsB.close();
+    
+}
 
 int WavetableOscillator::getOutputCount() {
     return 1;
